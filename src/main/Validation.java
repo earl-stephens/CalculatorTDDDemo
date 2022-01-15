b7f7e3a61ce1ea2065c8a3e338c0eff82403ba7f
earl-stephens <stephensearl@hotmail.com>
2022-01-14 22:56:04 -0700

    Create a validate method

--- src/main/Validation.java
+++ src/main/Validation.java
@@ -1,5 +1,11 @@
 package main;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Validation {
+	public List<String> validate(String... values) {
+		return Collections.EMPTY_LIST;
+	}
 
 }
--- src/test/ValidationTest.java
+++ src/test/ValidationTest.java
@@ -1,16 +1,39 @@
 package test;
 
+import java.util.List;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import main.Validation;
 
+/* Create a validate method which returns a
+ * List<String> of errors and accepts a vargs
+ * array of Strings - signature will be public
+ * List<String> validate(String... values).
+ * Write a unit test which returns an empty
+ * list.
+ */
+
 public class ValidationTest {
+	private Validation validation;
+	
+	@Before
+	public void setUp() {
+		validation = new Validation();
+	}
 	
 	@Test
 	public void shouldReturnAnInstanceOfValidation() {
 		Validation validation = new Validation();
 		Assert.assertNotNull(validation);
+	}
+	
+	@Test
+	public void shouldReturnAnEmptyListWhenNoErrorsPassedIn() {
+		List<String> errors = validation.validate("1.11", "2.22");
+		Assert.assertTrue(errors.isEmpty());
 	}
 
 }
