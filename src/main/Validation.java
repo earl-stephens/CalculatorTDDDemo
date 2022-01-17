87516767e78c10255d121e82d0339c947eb381ed
earl-stephens <stephensearl@hotmail.com>
2022-01-16 22:37:49 -0700

    Attempt to check validate method

--- src/main/Validation.java
+++ src/main/Validation.java
@@ -1,10 +1,15 @@
 package main;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
 public class Validation {
 	public List<String> validate(String... values) {
+		BigDecimal firstValue = new BigDecimal(values[0]);
+		if(firstValue.scale() > 2) {
+			throw new IllegalArgumentException("Too many decimal places for value " + values[0]);
+		}
 		return Collections.EMPTY_LIST;
 	}
 
--- src/test/ValidationTest.java
+++ src/test/ValidationTest.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 import java.util.List;
 
 import org.junit.Assert;
@@ -8,12 +12,20 @@ import org.junit.Test;
 
 import main.Validation;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /* Create a validate method which returns a
  * List<String> of errors and accepts a vargs
  * array of Strings - signature will be public
  * List<String> validate(String... values).
  * Write a unit test which returns an empty
  * list.
+ * 
+ * Using TDD, pass a single String with decimal value
+ * 1.111 in to the validate method.  Assert that a 
+ * List<String> of size 1 is returned containing the message
+ * 'Too many decimal places for value 1.111'
  */
 
 public class ValidationTest {
@@ -34,6 +46,13 @@ public class ValidationTest {
 	public void shouldReturnAnEmptyListWhenNoErrorsPassedIn() {
 		List<String> errors = validation.validate("1.11", "2.22");
 		Assert.assertTrue(errors.isEmpty());
+	}
+	
+	@Test
+	public void shouldReturnAnErrorForTooManyDecimalPlaces() {
+		List<String> errors = validation.validate("1.111");
+		List<String> expected = Arrays.asList("Too many decimal places for value 1.11");
+		assertThat(errors, is(expected));
 	}
 
 }
